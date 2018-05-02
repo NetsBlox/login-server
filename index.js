f29373f7b4b932c172954679a20d7a233dd36034
Brian Broll <brian.broll@vanderbilt.edu>
2018-05-02 09:39:05 -0500

    Added server and login page

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-// Set up the login button
-const loginBtn = document.getElementById('login');
-const passwordField = document.getElementById('password');
-const usernameField = document.getElementById('username');
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 8000;
+const SERVER_ADDRESS = process.env.EDITOR_ADDRESS;
 
-loginBtn.onclick = function() {
-    // Get the username and password
-    const username = usernameField.value;
-    const password = passwordField.value;
+let app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('pug', require('pug').__express);
+app.set('views', path.join(__dirname, 'views'));
 
-    // Check if "remember me" is checked
-    //const remember = document.getElementById('remember').checked;
+console.log('authenticating with', SERVER_ADDRESS);
+app.get('/index.html', (req,res) => res.render('index.pug',{SERVER_ADDRESS: SERVER_ADDRESS}));
 
-    // Login!
-	const url = 'https://dev.netsblox.org';
-	const redirectUrl = window.location.search.slice(1).split('&')
-		.map(pair => {
-			const keyVal = pair.split('=');
-			keyVal[1] = decodeURIComponent(keyVal[1]);
-			return keyVal;
-		})
-		.find(pair => pair[0] === 'redirect')
-		.pop();
-
-	const auth = new AuthHandler(url);
-	auth.login(username, password)
-		// If login successful, redirect to the next url
-		.then(request => window.location = redirectUrl)
-		.catch(err => M.toast({html: `Login Failed: ${err.request.responseText}`}));
-};
-
-usernameField.onkeyup = event => {
-	event.preventDefault();
-	if (event.keyCode === 13) {
-		passwordField.focus();
-	}
-};
-
-passwordField.onkeyup = event => {
-	event.preventDefault();
-	if (event.keyCode === 13) {
-		loginBtn.click();
-	}
-};
-
+app.listen(PORT, err => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${PORT}`);
+});
